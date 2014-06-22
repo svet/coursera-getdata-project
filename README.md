3c4b1996fb73387244d797f4d915cde33f63c9c3
Svet Ivantchev <svet@efaber.net>
2014-06-22 10:15:47 +0200

    Updated path to data, CodeBook and Readme

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 Coursera getdata project
 ========================
 
-# Cleaning the data
+## run_analysis.R script
+
+The run_analysis.R script performs the following:
+
+1. Reads the human-readable variable names from the features.txt file.
+2. Cleans the variables names strings removing characters "(", ")" and ",". This way these names can be used as column names in a dataframe.
+3. Reads the train dataset from test/X_test.txt, test/subject_test.txt and test/y_test.txt files.
+4. Binds the data from all three files in single dataframe
+5. Repeats steps 2-4 for traing data.
+6. Creates complete dataset including all traing and test data.
+7. Convert the data in "activity" column from integer to Factor with human-readable labels
+8. Creates a list of variables which contains "Mean", "mean" or "std" in their names
+9. Creates a new tidy dataset with all the observations and only columns selected in the previous step, "subject" and "activity"
+10. Saves the new tidy dataset to file named "tidy1.txt". To read this file use read.table("tidy1.txt", header=TRUE)
+
+11. Creates second dataset tidy2.txt
 
 
+## CodeBook.md
+
+Codebook describing the variables in tidy1.txt and tidy2.txt files
+
+## How to run
+
+To repeat the cleaning procedure execute the run_analysis.R:
+
+   mkdir clean; cd clean
+   git clone https://github.com/svet/coursera-getdata-project/
+   curl -o uci.zip https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+   unzip uci.zip 
+   R CMD BATCH coursera-getdata-project/run_analysis.R 
+
+   
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,27 +1,29 @@
+data_path <- "./UCI HAR Dataset/"
+
 # read the variable names and remove problematic characters ( "(", ")" and "," )
-var_names <- read.table("./features.txt", header=FALSE, stringsAsFactors=FALSE)
+var_names <- read.table(paste(data_path,"features.txt",sep=""), header=FALSE, stringsAsFactors=FALSE)
 var_names$V2 <- gsub("[\\(\\)]", "", var_names$V2)
 var_names$V2 <- gsub("\\,", "-", var_names$V2)
 
 # read test set
-X_test <- read.table("./test/X_test.txt", header=FALSE)
+X_test <- read.table(paste(data_path,"test/X_test.txt",sep=""), header=FALSE)
 # assign human-readble names to each column
 names(X_test) <- var_names$V2
 # read the subject id and name the column "subject" insted of "V1"
-subject_test <- read.csv("./test/subject_test.txt", header=FALSE)
+subject_test <- read.csv(paste(data_path,"test/subject_test.txt",sep=""), header=FALSE)
 names(subject_test)=c("subject")
 # read the activity values and name the column "activity" insted of "V1"
-y_test <- read.csv("./test/y_test.txt", header=FALSE)
+y_test <- read.csv(paste(data_path,"test/y_test.txt",sep=""), header=FALSE)
 names(y_test)=c("activity")
 # combine the columns for complete test set
 all_test <- cbind(X_test, subject_test, y_test)
 
 # repeat the same steps for the training set
-X_train <- read.csv("./train/X_train.txt", sep="", header=FALSE)
+X_train <- read.csv(paste(data_path,"/train/X_train.txt",sep=""), sep="", header=FALSE)
 names(X_train) <- var_names$V2
-subject_train <- read.csv("./train/subject_train.txt", header=FALSE)
+subject_train <- read.csv(paste(data_path,"train/subject_train.txt",sep=""), header=FALSE)
 names(subject_train)=c("subject")
-y_train <- read.csv("./train/y_train.txt", header=FALSE)
+y_train <- read.csv(paste(data_path,"train/y_train.txt",sep=""), header=FALSE)
 names(y_train)=c("activity")
 all_train <- cbind(X_train, subject_train, y_train)
 
@@ -40,6 +42,6 @@ columns_to_keep <- c(columns_to_keep, "subject", "activity")
 reduced_set <- all[columns_to_keep]
 
 # write the final dataset as text file, to read it back use 
-#       rr <- read.table("./tidy1.txt", header=TRUE)
+#       rr <- read.table("tidy1.txt", header=TRUE)
 write.table(reduced_set, "tidy1.txt", row.names = FALSE, col.names = TRUE, quote = FALSE)
 
